7085b55b5d5478578594e2985e3a62bd3379a46c
JaloliddinKulmamatov <itjalol2010@gmail.com>
2023-12-12 23:07:01 +0500

    jalol

---
+++ js/homework.js
@@ -0,0 +1,105 @@
+// if1
+
+// let a = 3;
+
+// if (a >0) {
+//     console.log(++a);
+// }else {
+//     console.log(a);
+// }
+
+//if2
+
+// let a = 0;
+
+// if (a > 0) {
+//     console.log(++a);
+// }else if (a < 0)(
+//     console.log(a - 2)
+// )
+// else if (a == 0){
+//     console.log(a = 10);
+// }
+
+
+//if 4
+
+// a = 4;
+// b = 5;
+
+// if (a > b) {
+//     console.log(a);
+// }else {
+//     console.log(b);
+// }
+
+
+//if 5
+
+// a = 6;
+// b = 1;
+
+// if (a > b) {
+//     console.log(b);
+// }else {
+//     console.log(a);
+// }
+
+//if 6
+
+// a = 9;
+// b = 4;
+
+// if (a > b) {
+//     console.log(a);
+//     console.log(b);
+// }else {
+//     console.log(b);
+//     console.log(a);
+// }
+
+//if7
+
+// a = 5;
+// b = 2;
+
+// if (a < b) {
+//     console.log(a);
+//     console.log(b);
+// }else {
+//     console.log(b);
+//     console.log( a = a - b);
+// }
+
+//if8
+
+// let a = 3;
+// let b = 8;
+// let k = 0;
+
+// if (a != b) {
+//     console.log(k = a + b); //A
+//     console.log(b = a + b); //B
+// }else if (a == b) {
+//     console.log(a = 0);
+//     console.log(b = o);
+// }
+
+
+//if9
+
+// let a = 8;
+// let b = 8;
+
+// if (a != b) {
+//     if (a > b) {
+//         console.log(a = a);
+//         console.log(b = a);
+//     }else {
+//         console.log(b = b);
+//         console.log(a = b);
+//     }
+// }else if (a == b) {
+//     console.log(a = 0);
+//     console.log(b= 0);
+// }
